8095502fa59ccf8d63b5b4ca2e927c27fe4f7349
mucnjakf <filip.mucnjak@outlook.com>
2024-03-10 13:01:05 +0100

    Implemented entity type configurations

---
+++ src/CashFlow.Database/Constants/TableNames.cs
@@ -0,0 +1,8 @@
+﻿namespace CashFlow.Database.Constants;
+
+internal static class TableNames
+{
+    internal const string Account = "Accounts";
+    internal const string Transaction = "Transactions";
+    internal const string Category = "Categories";
+}
---
+++ src/CashFlow.Database/EntityConfiguration/AccountEntityTypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using CashFlow.Core.Entities;
+using CashFlow.Database.Constants;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CashFlow.Database.EntityConfiguration;
+
+internal sealed class AccountEntityTypeConfiguration : IEntityTypeConfiguration<Account>
+{
+    public void Configure(EntityTypeBuilder<Account> builder)
+    {
+        builder.ToTable(TableNames.Account);
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.CreatedUtc).IsRequired();
+
+        builder.Property(x => x.UpdatedUtc).IsRequired();
+
+        builder.Property(x => x.Balance).IsRequired();
+
+        builder
+            .HasMany(x => x.Transactions)
+            .WithOne(x => x.Account)
+            .HasForeignKey(x => x.AccountId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
