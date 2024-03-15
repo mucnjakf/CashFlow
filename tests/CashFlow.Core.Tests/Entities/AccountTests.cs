00ce16ce3d4884b742f7a61b69eac9ed50fc1f88
mucnjakf <filip.mucnjak@outlook.com>
2024-03-15 23:38:45 +0100

    Implemented basic entities tests

---
+++ tests/CashFlow.Core.Tests/Entities/AccountTests.cs
@@ -0,0 +1,40 @@
+﻿using CashFlow.Core.Constants;
+using CashFlow.Core.Entities;
+using CashFlow.Core.Exceptions;
+using FluentAssertions;
+
+namespace CashFlow.Core.Tests.Entities;
+
+public sealed class AccountTests
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    public void Create_Should_ReturnAccount_When_ArgumentsAreValid(int balance)
+    {
+        // Act
+        Account account = Account.Create(balance);
+
+        // Assert
+        account.Should().NotBeNull();
+        account.Id.Should().NotBeEmpty();
+        account.CreatedUtc.Should().NotBeBefore(DateTime.UtcNow.AddSeconds(-5));
+        account.UpdatedUtc.Should().NotBeBefore(DateTime.UtcNow.AddSeconds(-5));
+        account.Balance.Should().BeGreaterOrEqualTo(0);
+        account.Balance.Should().Be(balance);
+        account.Transactions.Should().BeNull();
+    }
+
+    [Fact]
+    public void Create_Should_ThrowAccountException_When_ArgumentsAreInvalid()
+    {
+        // Arrange
+        const int balance = -1;
+
+        // Act
+        Func<Account> result = () => Account.Create(balance);
+
+        // Assert
+        result.Should().ThrowExactly<AccountException>().WithMessage(Errors.Account.BalancePositiveNumber);
+    }
+}
---
+++ tests/CashFlow.Tests/Data/Entities/CategoryData.cs
@@ -0,0 +1,11 @@
+﻿using CashFlow.Core.Entities;
+
+namespace CashFlow.Tests.Data.Entities;
+
+public static class CategoryData
+{
+    public static Category GetCategory()
+    {
+        return Category.Create("Shopping");
+    }
+}
