2314fb56bb20e4561c5f8b5c7b518f6bfcf16e21
mucnjakf <filip.mucnjak@outlook.com>
2024-03-18 22:13:20 +0100

    Implemented http exception

---
+++ src/CashFlow.Core/Exceptions/HttpException.cs
@@ -0,0 +1,8 @@
+﻿using System.Net;
+
+namespace CashFlow.Core.Exceptions;
+
+public class HttpException(HttpStatusCode httpStatusCode, string message) : Exception(message)
+{
+    public HttpStatusCode HttpStatusCode { get; private set; } = httpStatusCode;
+}
