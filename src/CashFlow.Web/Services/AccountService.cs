6f013e549c831a8b8e682643e779bbd4147a8c37
mucnjakf <filip.mucnjak@outlook.com>
2024-03-24 16:30:42 +0100

    Implemented services

---
+++ src/CashFlow.Web/Services/AccountService.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http.Json;
+using CashFlow.Web.Commands;
+using CashFlow.Web.Dtos;
+using CashFlow.Web.Services.Abstract;
+
+namespace CashFlow.Web.Services;
+
+internal sealed class AccountService(HttpClient httpClient) : IAccountService
+{
+    public async Task<AccountDto> GetAccountAsync()
+    {
+        HttpResponseMessage httpResponseMessage = await httpClient.GetAsync("account");
+
+        if (!httpResponseMessage.IsSuccessStatusCode)
+        {
+            throw new Exception($"Status code {httpResponseMessage.StatusCode}");
+        }
+
+        AccountDto? account = await httpResponseMessage.Content.ReadFromJsonAsync<AccountDto>();
+
+        if (account is null)
+        {
+            throw new Exception("Unable to parse account DTO");
+        }
+
+        return account;
+    }
+
+    public async Task UpdateAccountAsync(UpdateAccountCommand command)
+    {
+        HttpResponseMessage httpResponseMessage = await httpClient.PutAsJsonAsync("account", command);
+
+        if (!httpResponseMessage.IsSuccessStatusCode)
+        {
+            throw new Exception($"Status code {httpResponseMessage.StatusCode}");
+        }
+    }
+}
